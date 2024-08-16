28e51090fb2513182a5d757e4ecd604bed612a73
shi_guowei <shiguowei333@outlook.com>
2024-08-16 16:07:26 +0800

    电梯导航向下滚动300像素显示电梯导航栏

--- js/电梯导航案例.js
+++ js/电梯导航案例.js
@@ -1,9 +1,20 @@
 window.addEventListener('scroll', e => {
-    console.log(document.documentElement.scrollTop)
-    if(document.documentElement.scrollTop >= 300) {
-        console.dir(document.querySelector('.temp'))
-        document.querySelector('.temp').style.display = 'block'
-    }else if (document.documentElement.scrollTop < 300) {
-        document.querySelector('.temp').style.display = 'none'
+    console.log(window.scrollY)
+    if(window.scrollY >= 300) {
+        document.querySelector('.xtx-elevator').style.opacity = 1
+    }else {
+        document.querySelector('.xtx-elevator').style.opacity = 0
     }
 })
+
+
+
+// window.addEventListener('scroll', e => {
+//     console.log(document.documentElement.scrollTop)
+//     if(document.documentElement.scrollTop >= 300) {
+//         console.dir(document.querySelector('.temp'))
+//         document.querySelector('.temp').style.display = 'block'
+//     }else if (document.documentElement.scrollTop < 300) {
+//         document.querySelector('.temp').style.display = 'none'
+//     }
+// })
