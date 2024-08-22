8c9cceb9dd0e1670849b5519944b53bf1da74d3f
shi_guowei <shiguowei333@outlook.com>
2024-08-22 17:56:42 +0800

    滚动页面自动匹配对应的电梯栏为显示状态

--- js/电梯导航案例.js
+++ js/电梯导航案例.js
@@ -19,3 +19,19 @@ document.querySelector('.xtx-elevator-list').addEventListener('click', e => {
         }
     }
 }) 
+
+window.addEventListener('scroll', e => {
+    console.log(document.querySelector('.xtx-elevator li:nth-child(2) a'))
+    if(document.querySelector('.xtx-elevator a.active')) {
+        document.querySelector('.xtx-elevator a.active').classList.remove('active')
+    }
+    if(window.scrollY >= document.querySelector('.xtx_goods_new').offsetTop && window.scrollY < document.querySelector('.xtx_goods_popular').offsetTop) {
+        document.querySelector('.xtx-elevator li:nth-child(1) a').classList.add('active')
+    }else if(window.scrollY >= document.querySelector('.xtx_goods_popular').offsetTop && window.scrollY < document.querySelector('.xtx_goods_brand').offsetTop) {
+        document.querySelector('.xtx-elevator li:nth-child(2) a').classList.add('active')
+    }else if(window.scrollY >= document.querySelector('.xtx_goods_brand').offsetTop && window.scrollY < document.querySelector('.xtx_goods_topic').offsetTop) {
+        document.querySelector('.xtx-elevator li:nth-child(3) a').classList.add('active')
+    }else if(window.scrollY >= document.querySelector('.xtx_goods_topic').offsetTop && window.scrollY < document.querySelector('.xtx_footer').offsetTop) {
+        document.querySelector('.xtx-elevator li:nth-child(4) a').classList.add('active')
+    }
+})
